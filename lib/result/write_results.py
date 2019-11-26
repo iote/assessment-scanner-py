597d086a1c1f3925d0b03ffb982a6b2b7f49defb
jrosseel <rosseeljente@gmail.com>
2019-11-26 07:03:16 +0100

    Move back to local write
    The trick is to mount the filesystem as a distributed drive

--- lib/result/write_results.py
+++ lib/result/write_results.py
@@ -1,7 +1,5 @@
 import json
 import cv2
-from google.cloud import storage
-from tempfile import TemporaryFile
 
 # Function that writes the result in a json object
 # Arguments:
@@ -12,9 +10,6 @@ def write(student_filled_box, algo_result, output_dir):
   data = {}
   data['reads'] = len(algo_result);
   results = []
-
-  client = storage.Client()
-  bucket = client.get_bucket('assessment-results')
 
   # Structure of result (i, has_value, result_image, x, w, y, h, value?)
   for index, result in enumerate(algo_result):
@@ -41,16 +36,13 @@ def write(student_filled_box, algo_result, output_dir):
     }
 
     if (has_value):
-      output_loc = output_dir + 'num_' + str(i)
+      output_loc = output_dir + 'num_' + str(i);
 
-      save_image(image, bucket, output_loc + '.jpg')
-      # cv2.imwrite(output_loc + '.png', image)
-      value['loc'] = output_loc + '.jpg'
+      cv2.imwrite(output_loc + '.png', image)
+      cv2.imwrite(output_loc + '_ml.png', result_image)
 
-      # TODO - ML Pipeline
-      # save_image(result_image, bucket, output_loc + '_ml.png')
-      # cv2.imwrite(output_loc + '_ml.png', result_image)
-      # value['loc_ml'] = output_loc + '_ml.png'
+      value['loc'] = output_loc + '.png'
+      value['loc_ml'] = output_loc + '_ml.png'
 
       value['value'] = result[8]
 
@@ -58,30 +50,15 @@ def write(student_filled_box, algo_result, output_dir):
 
   data['results'] = results
 
-  student_overview_loc = output_dir + 'student_overview.jpg'
-  # cv2.imwrite(student_overview_loc, student_filled_box)
-  save_image(student_filled_box, bucket, student_overview_loc)
-
+  student_overview_loc = output_dir + 'student_overview.png'
+  cv2.imwrite(student_overview_loc, student_filled_box)
   data['overview'] = student_overview_loc
 
+  json_result_object = json.dumps(data)
+
   # Write JSON Result File
-  result_loc = output_dir + 'result.json'
-  save_json(data, bucket, result_loc)
+  f = open(output_dir + 'result.json', "w")
+  f.write(json_result_object)
+  f.close()
 
-  return result_loc
-
-
-def save_image(image, bucket, file_name):
-  with TemporaryFile() as gcs_image:
-    image.tofile(gcs_image)
-    gcs_image.seek(0)
-    blob = bucket.blob(file_name)
-    blob.upload_from_file(gcs_image)
-
-def save_json(json_data, bucket, file_name):
-  with TemporaryFile() as gcs_file:
-    gcs_file.write(json.dumps(json_data))
-
-    gcs_file.seek(0)
-    blob = bucket.blob(file_name)
-    blob.upload_from_file(gcs_file)
+  return output_dir + 'result.json'
