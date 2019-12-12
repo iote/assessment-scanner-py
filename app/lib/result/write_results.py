19db348e2d399ed220cb9c16f7f1b8a0d9ffa2c9
jrosseel <rosseeljente@gmail.com>
2019-12-12 13:23:48 +0300

    Combine images into spritesheet

--- app/lib/result/write_results.py
+++ app/lib/result/write_results.py
@@ -1,6 +1,7 @@
 import json
 import cv2
 import os, sys
+import numpy as np
 
 # Function that writes the result in a json object
 # Arguments:
@@ -34,7 +35,9 @@ def write(student_filled_box, algo_result, subj_group, stream, assessment, sheet
   data['reads'] = len(algo_result);
   results = []
 
-  # Structure of result (i, has_value, result_image, x, w, y, h, value?)
+  spritesheet = [];
+
+  # Structure of result (i, has_value, result_image, ml_image, x, w, y, h, value?)
   for index, result in enumerate(algo_result):
 
     i = result[0]
@@ -45,7 +48,7 @@ def write(student_filled_box, algo_result, subj_group, stream, assessment, sheet
     w            = result[5]
     y            = result[6]
     h            = result[7]
-
+    
     value = {
         'index': i,
 
@@ -68,13 +71,21 @@ def write(student_filled_box, algo_result, subj_group, stream, assessment, sheet
 
       value['value'] = result[8]
 
+      # Add to spritesheet
+      spritesheet = np.concatenate((spritesheet, result_image), axis=0)
+
     results.append(value)
 
   data['results'] = results
 
   student_overview_loc = output_dir + '/student_overview.png'
   cv2.imwrite(student_overview_loc, student_filled_box)
+
+  # Add overview to spritesheet
+  spritesheet = np.concatenate((spritesheet, student_filled_box), axis=1)
+
   data['overview'] = student_overview_loc
+  data['spritesheet'] = output_dir + '/total_overview.png'
   data['result_loc'] = output_dir + '/result.json'
   json_result_object = json.dumps(data)
 
