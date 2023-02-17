0af6748327c350bfefc8451e2d50c2fdcd505819
Jente Rosseel <rosseeljente@gmail.com>
2023-02-17 18:22:18 +0300

    :wrench: Refactor test.py

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # elewa-assessment-scanner
 
 Project that contains the code to scan assessments. Packages its own Python compiler and 
+
+
+
+
+## Setup
+
+1. Install Python 3
+2. pip3 install -r requirements.txt
+3. python app/test.py
+
+Some deps might not be downloaded that easily. In case of package-not-found issues, use following cmds for certain packages:
+
+### Tensorflow
+
+`pip3 install tensorflow-macos`
+
+### OpenCV
+
+`pip3 install opencv-python`
--- app/test.py
+++ app/test.py
@@ -1,4 +1,5 @@
 from lib import extract_numbers
+import cv2
 
 # import warnings filter tc ignore all future warnings
 from warnings import simplefilter
@@ -8,7 +9,11 @@ import tensorflow as tf
 print('Testing app with test input located at _test/input.png. Writing results to _debug')
 tf_interpreter = tf.lite.Interpreter(model_path="model/handwriting_read.tflite")
 #
-# Execute script and return results via stdout
-extract_numbers.run('_test/test_6.jpg', '_debug/', tf_interpreter)
+# Configure target image here
+img = cv2.imread('../_test/test_6.jpg', 0)
+
+# Run the test
+extract_numbers.run(img, 'some_subject', 'class_1', 'stream_1', 1, tf_interpreter, 1)
 
 print('Test done. Check _debug folder.')
+
